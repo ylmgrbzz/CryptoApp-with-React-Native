ea9d09171fa6622dc6758c68e3283643ec8648f8
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-05 21:09:30 +0300

    CrpytoApp with ReactNative

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on yousr app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const fetchData = () => {
+  return fetch(
+    "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    {
+      method: "GET",
+      headers: {
+        "X-CMC_PRO_API_KEY": "b45eff95-a8f6-426d-84b1-76662c5d54f1",
+      },
+    }
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const App = () => {
+  const [data, setData] = useState([0]);
+
+  useEffect(() => {
+    fetchData()
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        console.log(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return null;
+};
+
+export default App;
